6cd19ed67d03407a17b891f1e23f6be0e4c56636
Helena Martin <heleenamartiin@gmail.com>
2025-07-01 17:30:12 +0200

    EX_01 actualizado

--- EX_01/print_comb.py
+++ EX_01/print_comb.py
@@ -1,31 +1,37 @@
-def print_comb(invertido):
-    """Genera todas las combinaciones de tres dígitos (del 0 al 9) sin repetir dígitos"""
-    result = ""
+def print_comb(invertido=False):
+    """Genera todas las combinaciones de tres dígitos (del 0 al 9) sin repetir dígitos, en orden ascendente.
+    Si invertido=True, las muestra en orden descendente.
+    """
+    combinaciones = []
 
-    if invertido == False:
-        for i in range(10):
-            for j in range (i+1, 10):
-                for k in range (j+1, 10):
-                    result += (f'{i}{j}{k}, ')      
-    else:
-        for k in range(9, -1, -1):
-            for j in range (k-1, -1, -1):
-                for i in range (j-1, -1, -1):
-                    result += (f'{i}{j}{k}, ')
+    for i in range(10):
+        for j in range(i + 1, 10):
+            for k in range(j + 1, 10):
+                combinaciones.append(f"{i}{j}{k}")     
     
-    # eliminar el último ', '
-    result = result[:-2]
-    print(result)
+    if invertido:
+        for i in range(len(combinaciones) - 1, -1, -1):
+            if i > 0:
+                print(combinaciones[i], end=", ")
+            else:
+                print(combinaciones[i], end="")
+    else:
+        for i in range(len(combinaciones)):
+            if i < len(combinaciones) - 1:
+                print(combinaciones[i], end=", ")
+            else:
+                print(combinaciones[i], end="") 
+
 
 
 def main():
-    invertido = input("¿Quieres ver las combinaciones en orden invertido? (s/n): ")
+    invertido = input("¿Quieres ver las combinaciones en orden invertido? (s/n): ").lower().strip()
     if invertido == "s":
         invertido = True
     elif invertido == "n":
         invertido = False
     else:
-        print("ERROR -> Los valores válidos son: s/n")
+        print("Entrada no válida. Debes introducir 's' o 'n'.")
         return
     
     print_comb(invertido)
