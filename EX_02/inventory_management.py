c056395a3235c92cf729185dbcbbd989af98841b
Helena Martin <heleenamartiin@gmail.com>
2025-07-01 17:10:55 +0200

    Menu

--- EX_02/inventory_management.py
+++ EX_02/inventory_management.py
@@ -1,60 +1,58 @@
 
 class InventoryManagement:
-    def __init__(self, inventory={}):
+    def __init__(self, inventory=None):
         """Iniciliza los atributos
         Argumentos posicionales:
         inventory - diccionario que almacena {producto: cantidad} 
         """
-        self._inventory = inventory
+        if inventory is None:
+            self._inventory = {}
+        else:
+            self._inventory = inventory
     
     def add_product(self, product, quantity):
-        print(f"\nAñadir al inventario {product}-> Cantidad {quantity}:")
-        if quantity < 0 or type(quantity) != int:
-            print("ERROR: La cantidad debe ser un número entero positivo")
-            return
+        if quantity < 0 :
+            raise ValueError()
 
         if product in self._inventory:
             self._inventory[product] += quantity
-            print(f"{product} ya existe. Se ha añadido {quantity} más")
+            print(f"{product} ya existe. Se ha añadido {quantity} más.")
         else:
             self._inventory[product] = quantity
-            print(f"Se ha añadido correctamente")
+            print(f"Se ha añadido correctamente.")
     
     def delete_product(self, product):
-        print(f"\nEliminar {product}:")
         if product in self._inventory:
             self._inventory.pop(product)
-            print(f"{product} se ha eliminado correctamente")
+            print(f"{product} se ha eliminado correctamente.")
         else:
-            print(f"ERROR: {product} no encontrado para eliminar")
+            print(f"ERROR: {product} no encontrado para eliminar.")
 
     def consult_product(self, product):
-        print(f"\nConsultar {product}:")
         if product in self._inventory:
             print(f"La cantidad de {product} es {self._inventory.get(product)}")
         else:
-            print(f"ERROR: {product} no existe en el inventario")
+            print(f"ERROR: {product} no existe en el inventario.")
 
     def mod_quantity(self, product, new_quantity):
-        print(f"\nModificar cantidad de {product} por {new_quantity}:")
-        if new_quantity <0 or type(new_quantity) != int:
-            print("ERROR: la cantidad ha de ser un número positivo entero")
-            return
+        if new_quantity < 0:
+            raise ValueError()
         if product in self._inventory:
             self._inventory[product] = new_quantity
-            print(f"Se ha modificado correctamente")
+            print(f"Se ha modificado correctamente.")
         else:
-            print(f"ERROR: {product} no existe en el inventario")
+           raise KeyError()
 
     def find_product(self, text):
-        print(f"\nBuscar '{text}'")
-        idx = 0
+        text = text.lower().strip()
+        found = False
+        print("------------------")
         for elem in self._inventory:
-            if text in elem:
+            if text in elem.lower():
                 print(f'{elem}: {self._inventory[elem]}')
-                idx = 1
-        if idx == 0:
-            print(f"No se han encontrado resultados para '{text}'")
+                found = True
+        if not found:
+            print(f"No se han encontrado resultados para '{text}'.")
 
     def view_inventory(self, sort=False):
         print("\n--- Inventario ---")
@@ -65,41 +63,5 @@ class InventoryManagement:
 
 
 
-tienda = InventoryManagement()
 
-print("\nInventario inicial:")
-tienda.view_inventory() 
-
-# Añadir Producto
-tienda.add_product("Manzanas", 10)
-tienda.view_inventory()
-
-tienda.add_product("Peras", 5)
-tienda.view_inventory()
-
-tienda.add_product("Aguacates", 70)
-
-tienda.add_product("Bananos", 4)
-
-tienda.add_product("Guayabas", 7)
-
-tienda.add_product("Manzanas", 5)  
-tienda.view_inventory()
-tienda.view_inventory(sort=True) 
-
-# Consultar Producto
-tienda.consult_product("Manzanas")
-
-# Eliminar Producto
-tienda.delete_product("Peras")
-tienda.view_inventory()
-
-# Modificar cantidad de Producto
-tienda.mod_quantity("Manzanas", 50)
-tienda.view_inventory()
-
-# Buscar Producto
-tienda.find_product("Ma")
-tienda.find_product("a")
-tienda.find_product("txt")
 
---
+++ EX_02/menu.py
@@ -0,0 +1,67 @@
+import inventory_management as im
+
+def mostrar_menu():
+    tienda = im.InventoryManagement()
+
+    while True:
+        print("\n-------- Menú --------\n")
+        print("1. Añadir productos")
+        print("2. Consultar uno")
+        print("3. Eliminar uno")
+        print("4. Modificar cantidades")
+        print("5. Buscar por texto")
+        print("6. Mostrar inventario")
+        print("7. Salir del programa")
+        print("\n----------------------")
+        
+        try:
+            opcion = int(input("\n¿Qué desea hacer? Seleccione un número (1-7): "))
+            if opcion < 1 or opcion > 7:
+                raise ValueError()
+        except ValueError:
+            print("Por favor, introduzca un número válido entre 1 y 7.")
+            continue
+        
+        if opcion == 1:
+            product = input("\n¿Qué producto quiere añadir?: ").capitalize().strip()
+            try:
+                quantity = int(input("Cantidad: "))
+                tienda.add_product(product, quantity)
+            except ValueError:
+                print("ERROR: La cantidad debe ser un número entero positivo.")
+        elif opcion == 2:
+            product = input("\n¿Qué producto quiere consultar?: ").capitalize().strip()
+            tienda.consult_product(product)
+        elif opcion == 3:
+            product = input("\n¿Qué producto quiere eliminar?: ").capitalize().strip()
+            tienda.delete_product(product)
+        elif opcion == 4:
+            product = input("\n¿De qué producto quiere modificar las cantidades?: ").capitalize().strip()
+            if product not in tienda._inventory:
+                print(f"ERROR: {product} no existe en el inventario.")
+                continue
+            else:         
+                try:           
+                    quantity = int(input("Nueva cantidad: "))
+                    tienda.mod_quantity(product, quantity)
+                except ValueError:
+                    print("ERROR: La cantidad ha de ser un número positivo entero.")
+        elif opcion == 5:
+            busqueda = input("\nBúsqueda por: ").strip()
+            tienda.find_product(busqueda)
+        elif opcion == 6:
+            sort = input("¿Quiere ver el inventario ordenado? (s/n): ").lower().strip()
+            if sort == 's':
+                tienda.view_inventory(sort)
+            elif sort == 'n':
+                tienda.view_inventory()
+            else:
+                print("ERROR: Por favor, introduce un valor válido (s/n).")
+                continue
+        else:
+            print("Saliendo...")
+            break
+
+
+if __name__ == "__main__":
+    mostrar_menu()
