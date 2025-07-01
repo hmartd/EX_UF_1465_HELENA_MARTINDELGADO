8ca321b498aba6eef54cb5724d4d5d79d98ae593
Helena Martin <heleenamartiin@gmail.com>
2025-07-01 12:18:39 +0200

    Subida EX_01 parte II

--- EX_01/print_comb.py
+++ EX_01/print_comb.py
@@ -1,21 +1,34 @@
-def print_comb(invertido=False):
+def print_comb(invertido):
     """Genera todas las combinaciones de tres dígitos (del 0 al 9) sin repetir dígitos"""
     result = ""
 
-    for i in range(10):
-        for j in range (i+1, 10):
-            for k in range (j+1, 10):
-                result += (f'{i}{j}{k}, ')
+    if invertido == False:
+        for i in range(10):
+            for j in range (i+1, 10):
+                for k in range (j+1, 10):
+                    result += (f'{i}{j}{k}, ')      
+    else:
+        for k in range(9, -1, -1):
+            for j in range (k-1, -1, -1):
+                for i in range (j-1, -1, -1):
+                    result += (f'{i}{j}{k}, ')
     
     # eliminar el último ', '
     result = result[:-2]
     print(result)
 
 
-
-
 def main():
-    print_comb()
+    invertido = input("¿Quieres ver las combinaciones en orden invertido? (s/n): ")
+    if invertido == "s":
+        invertido = True
+    elif invertido == "n":
+        invertido = False
+    else:
+        print("ERROR -> Los valores válidos son: s/n")
+        return
+    
+    print_comb(invertido)
 
 
 if __name__ == "__main__":
